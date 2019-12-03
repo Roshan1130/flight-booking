aace54139a2e3e17f622bda20565beb000abed3c
ashma9296 <ashma.khadka2019@gmail.com>
2019-12-03 16:04:45 -0500

    i add new service class for the price service

---
+++ src/main/java/service/com/PriceService.java
@@ -0,0 +1,13 @@
+package service.com;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+@Service
+public class PriceService {
+	
+	public void getticket() {
+		 System.out.println(" into the system");
+	}
+
+}
